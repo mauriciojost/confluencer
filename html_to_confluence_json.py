820fd367187b8a0c89d47fac38aa670eb0d15b52
Mauricio Jost <mauriciojost@gmail.com>
2020-04-04 17:05:49 +0200

    Minor refactoring

--- html_to_confluence_json.py
+++ html_to_confluence_json.py
@@ -6,15 +6,15 @@ page_filename = sys.argv[2]
 title = sys.argv[3]
 ancestor = int(sys.argv[4])
 
-with open(page_filename, 'r') as content_file:
-   content = content_file.read()
+with open(page_filename, 'r') as html_content_file:
+   html_content = html_content_file.read()
 
 print (json.dumps({
 "type":"page",
 "title":title,
 "ancestors":[{"id":ancestor}],
 "space": { "key":space_name },
-"body": { "storage":{ "value":content, "representation":"storage" }
+"body": { "storage":{ "value":html_content, "representation":"storage" }
 }
 }))
 
